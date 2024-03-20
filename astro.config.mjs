37e6bef694f077ccfad58f918ca49097524e2cd8
Adán Alvarez <geroshima123@gmail.com>
2024-03-19 23:25:57 -0300

    feat: add Tailwind integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
-  output: 'hybrid',
+  integrations: [react(), markdoc(), keystatic(), tailwind()],
+  output: 'hybrid'
 });
