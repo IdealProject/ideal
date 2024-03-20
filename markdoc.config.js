f8021048860a98c16536b77268e981b302c91a9b
Adán Alvarez <geroshima123@gmail.com>
2024-03-20 17:43:34 -0300

    feat: add markdoc.config.js file with LaTeX tag configuration

---
+++ markdoc.config.js
@@ -0,0 +1,13 @@
+import { defineMarkdocConfig, component } from "@astrojs/markdoc/config";
+
+export default defineMarkdocConfig({
+  tags: {
+    latex: {
+      render: component("@components/Latex.astro"), // Replace '@components/Latex.astro' with the actual path to your LaTeX component
+      attributes: {
+        // Define attributes if needed
+        formula: { type: String, required: true }, // Example attribute definition
+      },
+    },
+  },
+});
