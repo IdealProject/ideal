c2b5a94a39078155890de7217eb8affc5eb9a13b
Adán Alvarez <geroshima123@gmail.com>
2024-03-19 23:56:31 -0300

    feat: add @astrojs/cloudflare dependency and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import cloudflare from "@astrojs/cloudflare";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic(), tailwind()],
-  output: 'hybrid'
+  output: "server",
+  adapter: cloudflare(),
 });
