10ce08ac9a48711d94ae0bd2aac7bff47d03a862
Adán Alvarez <geroshima123@gmail.com>
2024-03-20 21:51:40 -0300

    update: add latex component in keystatic editor at keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,64 +1,97 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection, component } from "@keystatic/core";
+import Latex from "@components/Latex.astro";
 
 export default config({
   storage: {
-    kind: 'github',
-    repo: 'IdealProject/ideal'
+    kind: "github",
+    repo: "IdealProject/ideal",
   },
   //Puedo cambiar la ui del admin mode con ui:{}
   ui: {
     brand: {
-      name: 'Ideal Admin',
-
-    }
+      name: "Ideal Admin",
+    },
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         content: fields.document({
-          label: 'Content',
+          label: "Content",
           formatting: true,
           dividers: true,
           links: true,
           images: {
-            directory: 'src/assets/images/posts',
-            publicPath: '../../assets/images/posts/',
+            directory: "src/assets/images/posts",
+            publicPath: "../../assets/images/posts/",
+          },
+          componentBlocks: {
+            latex: component({
+              label: "LaTex Block",
+              schema: {
+                formula: fields.text({
+                  label: "LaTex Formula",
+                  description: "Enter your LaTex formula here",
+                  validation: {
+                    length: { min: 1 },
+                    isRequired: true,
+                  },
+                }),
+              },
+              preview: () => null,
+            }),
           },
         }),
-        authors: fields.array(fields.relationship({
-          label: 'Authors',
-          collection: 'authors',
-          validation: {
-            isRequired: true
-          },
-        }),
+        authors: fields.array(
+          fields.relationship({
+            label: "Authors",
+            collection: "authors",
+            validation: {
+              isRequired: true,
+            },
+          }),
           {
-            label: 'Authors',
-            itemLabel: (item) => item.value || "Please select a author"
-          })
+            label: "Authors",
+            itemLabel: (item) => item.value || "Please select a author",
+          }
+        ),
       },
     }),
     authors: collection({
-      label: 'Authors',
-      slugField: 'name',
-      path: 'src/content/authors/*',
-      format: { data: 'json' },
+      label: "Authors",
+      slugField: "name",
+      path: "src/content/authors/*",
+      format: { data: "json" },
       schema: {
-        name: fields.slug({ name: { label: 'Name' } }),
+        name: fields.slug({ name: { label: "Name" } }),
         avatar: fields.image({
-          label: 'Avatar',
-          directory: 'public/images/avatars',
-          publicPath: '/images/avatars',
+          label: "Avatar",
+          directory: "public/images/avatars",
+          publicPath: "/images/avatars",
         }),
         showcase: fields.blocks(
-          { link: { label: 'Link', schema: fields.object({ label: fields.text({ label: 'Label', validation: { length: { min: 1, } } }), url: fields.url({ label: 'URL' }) }), itemLabel: (item) => { return item.fields.label.value; } } },
-          { label: 'Tus links' })
-      }
+          {
+            link: {
+              label: "Link",
+              schema: fields.object({
+                label: fields.text({
+                  label: "Label",
+                  validation: { length: { min: 1 } },
+                }),
+                url: fields.url({ label: "URL" }),
+              }),
+              itemLabel: (item) => {
+                return item.fields.label.value;
+              },
+            },
+          },
+          { label: "Tus links" }
+        ),
+      },
     }),
   },
 });
