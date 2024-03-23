bb7d5e96efd333bf5054484fe2d7ec1589defbb4
Adán Alvarez <geroshima123@gmail.com>
2024-03-23 13:41:28 -0300

    feat: add astro-icon package and integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import cloudflare from "@astrojs/cloudflare";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), tailwind()],
+  integrations: [react(), markdoc(), keystatic(), tailwind(), icon()],
   output: 'hybrid',
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
