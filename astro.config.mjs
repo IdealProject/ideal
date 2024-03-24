53b95a3ff19aa6da13e629b7a2f928e1570a3729
Adán Alvarez <geroshima123@gmail.com>
2024-03-24 19:45:32 -0400

    feat: add robots.txt integration feat: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,25 @@ import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), tailwind(), icon()],
+  site: "https://ideal-7m3.sites.dev",
+  integrations: [
+    react(),
+    markdoc(),
+    keystatic(),
+    tailwind(),
+    icon(),
+    robotsTxt({
+      transform(content) {
+        return `${content}\nDisallow: ${site}/keystatic`;
+      },
+    }),
+  ],
   output: "hybrid",
   adapter: cloudflare(),
 });
