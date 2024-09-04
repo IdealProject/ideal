eecc0d6e56d3e4ea40bbfcffaac4a80507841067
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2024-09-04 18:16:01 -0400

    feat: changes in the system of materias completed

--- src/utils/data/dataInfor.ts
+++ src/utils/data/dataInfor.ts
@@ -2,25 +2,27 @@
 import { getCollection } from "astro:content";
 const datainfor = await getCollection("infor");
 
-export const datainfores = datainfor.map((e) => e.data.materias)
+const datainfores = datainfor.map((e) => e.data.materias)
 
+export const dataSemestresMaterias = datainfores.map(e => 
+    e.map((i: string, index: number) => ({
+        label: i,
+        value: index.toString()
+    })))
+    
+// export const materiaInfor1er = [
 
+//     { label: 'Fisica 1', value: 'F1' },
+//     { label: 'Arquitectura y Componentes', value: 'Arq1' },
+//     { label: 'Introducción a la Programación 1', value: 'P1' },
+//     { label: 'Comunicación Técnica 1', value: 'C1' },
+//     { label: 'Análisis 1', value: 'A1' },
+// ];
 
-
-
-
-export const materiaInfor1er = [
-    { label: 'Fisica 1', value: 'F1' },
-    { label: 'Arquitectura y Componentes', value: 'Arq1' },
-    { label: 'Introducción a la Programación 1', value: 'P1' },
-    { label: 'Comunicación Técnica 1', value: 'C1' },
-    { label: 'Análisis 1', value: 'A1' },
-];
-
-export const materiaInfor2do = [
-    { label: 'Fisica 2', value: 'F2' },
-    { label: 'Matemática Discreta', value: 'Dis' },
-    { label: 'Introducción a la Programación 2', value: 'P2' },
-    { label: 'Comunicación Técnica 2', value: 'C2' },
-    { label: 'Análisis Matemático 2', value: 'A2' },
-];
+// export const materiaInfor2do = [
+//     { label: 'Fisica 2', value: 'F2' },
+//     { label: 'Matemática Discreta', value: 'Dis' },
+//     { label: 'Introducción a la Programación 2', value: 'P2' },
+//     { label: 'Comunicación Técnica 2', value: 'C2' },
+//     { label: 'Análisis Matemático 2', value: 'A2' },
+// ];
--- src/utils/induDataHandler.ts
+++ src/utils/induDataHandler.ts
@@ -1,5 +1,9 @@
 import { fields } from "@keystatic/core";
-import { materiaInfor1er, materiaInfor2do } from "./data/dataInfor";
+// import { materiaInfor1er, materiaInfor2do } from "./data/dataInfor";
+
+import { dataSemestresMaterias} from "./data/dataInfor";
+
+
 import { semesterOptions } from "./data/dataSemester";
 
 export const induDataHanlder = fields.conditional(
@@ -14,71 +18,71 @@ export const induDataHanlder = fields.conditional(
     1: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[0],
+        defaultValue: '0'
     }
     ),
     2: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor2do,
-        defaultValue: 'F2'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     3: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     4: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     5: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     6: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     7: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     8: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     9: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
     10: fields.select({
         label: "Subject",
         description: "Enter the subject of the post",
-        options: materiaInfor1er,
-        defaultValue: 'F1'
+        options: dataSemestresMaterias[1],
+        defaultValue: '0'
     }
     ),
 }
