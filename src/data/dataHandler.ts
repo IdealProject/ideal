1992a9a2a2e2486ab4080bbaf695e1befe0ef9de
geroxima <geroshima123@gmail.com>
2024-09-06 13:39:26 -0400

    refactor: Update header navigation and search functionality

---
+++ src/data/dataHandler.ts
@@ -0,0 +1,69 @@
+import { fields } from "@keystatic/core";
+
+
+// Function that creates the initials for the value of a materia
+const initials = (cadena: string) => {
+    const words = cadena.split(' ')
+    const initials = words.map(e => e.charAt(0))
+    if (words.length === 1) { initials.push(words[0].charAt(1)) }
+
+    return initials.join('')
+}
+
+// Function that obtain a collection of the semesters and create a array with objects of the materias in the semester
+export const dataSemestreProcess = (data: any) => {
+    const dataCarrera = data.map((e: any) => e.data.materias)
+    return dataCarrera.map((e: any) =>
+        e.map((i: string) => ({
+            label: i,
+            value: initials(i)
+        })))
+}
+//Function that obtain a collection and make a array of the semesters in 
+export const dataSemestre = (data: any) => {
+    const dataCarrera = data.map((e: any) => e.data.name)
+    return dataCarrera.map((e: any, index: number) => ({
+        label: e,
+        value: (index + 1).toString()
+    }))
+}
+// We Check the amount of semester already created in the collection (major) and we create a array of posibles index for assign it in the fields select later 
+// if the amount of semester is less than 10 we add 0 to the array
+export const indexValues = (dataInforSemestres: Array<Array<String>>) => {
+    let result = [];
+    for (let i = 0; i < dataInforSemestres.length; i++) {
+        result.push(i);
+    }
+    if (dataInforSemestres.length < 10) {
+        for (let i = dataInforSemestres.length; i < 10; i++) {
+            result.push(0);
+        }
+    }
+    return result;
+}
+export const generateFieldsForSemesters = (dataMajor) => {
+    const semestersData = dataSemestreProcess(dataMajor);
+
+    const semesterNames = dataSemestre(dataMajor);
+    const indicesDisponibles = semestersData.map((_, index) => index); // Genera índices de acuerdo a la cantidad de semestres disponibles
+
+    return fields.conditional(
+        fields.select({
+            label: "Semestre",
+            description: "Select the semester",
+            options: semesterNames,
+            defaultValue: semesterNames[0].value,
+        }),
+        Object.fromEntries(
+            indicesDisponibles.map((index) => [
+                (index + 1).toString(),  // Mantén el índice alineado con los semestres
+                fields.select({
+                    label: "Subject",
+                    description: "Enter the subject of the post",
+                    options: semestersData[index] || [],
+                    defaultValue: semestersData[index] ? semestersData[index][0].value : ""
+                })
+            ])
+        )
+    );
+};
---
+++ src/data/dataMajor.ts
@@ -0,0 +1,7 @@
+export const majorOptions = [
+    { label: "Informática", value: "infor" },
+    { label: "Electromecánica", value: "electro" },
+    { label: "Civil", value: "civil" },
+    { label: "Industrial", value: "indu" },
+    { label: "CPA", value: "cpa" },
+];
