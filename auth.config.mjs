6d09504ebc986e66b27cf2fc41a1263a814d9a36
geroxima <geroshima123@gmail.com>
2025-04-19 15:41:46 -0300

    test: add cookie dependency

--- auth.config.mjs
+++ auth.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "auth-astro";
 import Google from '@auth/core/providers/google';
 
 export default defineConfig({
-    secret: import.meta.env.AUTH_SECRET,
     providers: [
         Google({
             clientId: import.meta.env.GOOGLE_CLIENT_ID,
