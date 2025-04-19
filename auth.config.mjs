39b2489c22af1389876b62b5dce01377441d8606
geroxima <geroshima123@gmail.com>
2025-04-19 15:19:08 -0300

    test: add secret configuration to auth setup

--- auth.config.mjs
+++ auth.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "auth-astro";
 import Google from '@auth/core/providers/google';
 
 export default defineConfig({
+    secret: import.meta.env.AUTH_SECRET,
     providers: [
         Google({
             clientId: import.meta.env.GOOGLE_CLIENT_ID,
